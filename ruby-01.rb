e2d948ab57d97388bbfb0035aa61b2bcb8e91efc
Tomas Valent <equivalent@eq8.eu>
2023-10-09 12:54:36 +0200

    shring lesson

--- ruby-01.rb
+++ ruby-01.rb
@@ -35,7 +35,6 @@ c
 c * 2
 #=> 14
 
-
 "abcd".class
 #=> String
 
@@ -52,7 +51,6 @@ c * 2
 
 "  abcd   ".strip
 #=> "abcd"
-
 
 "  abcd   ".strip.reverse.capitalize
 #=> "Dcba"
@@ -73,6 +71,9 @@ f = d.strip.upcase
 d
 #=> "  abcd   "
 
+d.strip!
+d
+# => "abcd"
 
 def say_hello
   "Hello"
@@ -81,9 +82,8 @@ end
 say_hello
 #=> Hello
 
-say_hello()
+say_hello
 #=> Hello
-
 
 def say_hello_to(name)
   "Hello #{name}"
@@ -117,94 +117,3 @@ say_hello_to_friends "John", "Paul", "George"
 
 say_hello_to_friends("John", "Paul", "George")
 #=> Hello John, Paul, George
-
-
-"a" + "b"
-#=> "ab"
-
-w = "b"
-
-"a" + w
-#=> "ab"
-
-"a" + 1
-#=> TypeError (no implicit conversion of Integer into String)
-# https://ruby-doc.org/core-3.1.0/String.html#method-i-2B
-
-"a" + 1.to_s
-#=> "a1"
-
-"a".+("b")
-#=> "ab"
-
-"a" * 3
-#=> "aaa"
-
-"a".*(3)
-#=> "aaa"
-
-3.times { puts "Hello" }
-# Hello
-# Hello
-# Hello
-# => 3
-
-def say_hello_world_maybe(name="World")
-  "Hello #{name}"
-end
-
-say_hello_world_maybe
-#=> Hello World
-
-say_hello_world_maybe("John")
-#=> Hello John
-
-def say_hello_world_or_i_kill_you(name)
-  "Hello #{name}"
-end
-
-say_hello_world_or_i_kill_you
-# =>  ArgumentError - wrong number of arguments (given 0, expected 1)
-
-say_hello_world_or_i_kill_you("World")
-# => "Hello World"
-
-
-ary = [1, 2, 3, 4]
-ary.class
-# => Array
-
-# https://ruby-doc.org/core-3.1.0/Array.html
-
-ary.sum
-# => 10
-
-ary.reverse
-# =>  [4, 3, 2, 1]
-
-ary
-# => [1, 2, 3, 4]
-
-ary2 = ary.reverse
-ary2
-# =>  [4, 3, 2, 1]
-ary
-# => [1, 2, 3, 4]
-
-ary.each { |item| puts item }
-# 1
-# 2
-# 3
-# 4
-#  => [1, 2, 3, 4]
-
-ary.each do |item|
-  puts item
-end
-# 1
-# 2
-# 3
-# 4
-#  => [1, 2, 3, 4]
-
-
